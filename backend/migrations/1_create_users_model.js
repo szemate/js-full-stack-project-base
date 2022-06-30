6c434e2bfa1b3da5cdf3e677a204aa6f53fd63f8
Máté Szendrő <szemate@posteo.net>
2022-06-30 08:39:44 +0200

    Add East MySQL adapter

--- backend/migrations/1_create_users_model.js
+++ backend/migrations/1_create_users_model.js
@@ -2,7 +2,7 @@ const db = require('../src/db');
 
 exports.migrate = async () => {
   await db.query(`
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE users (
       id INT AUTO_INCREMENT,
       name VARCHAR(255) NOT NULL,
       PRIMARY KEY (id)
@@ -11,5 +11,5 @@ exports.migrate = async () => {
 };
 
 exports.rollback = async () => {
-  await db.query('DROP TABLE IF EXISTS users');
+  await db.query('DROP TABLE users');
 };
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 
 if (process.env.NODE_ENV !== 'test') {
-  const databaseUrl = process.env.JAWSDB_URL || process.env.DATABASE_URL;
+  const databaseUrl = process.env.JAWSDB_URL // Heroku
+    || process.env.DATABASE_URL;
 
   if (!databaseUrl) {
     throw new Error('Missing environment variable DATABASE_URL');
